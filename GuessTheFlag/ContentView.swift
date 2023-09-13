d9b607a15ab83ae11dda40e6acefa72be2d68405
Eugene Cross <eugene.cross@gmail.com>
2023-09-13 11:32:16 +0100

    Day 75 accessibility improvements for voiceover

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -48,6 +48,20 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     @State private var selectedFlag = -1
     
     var body: some View {
@@ -90,7 +104,8 @@ struct ContentView: View {
                                 // animationAmount as we are animating the label not the button itself.  Adding the closure results in animating all 3 at the same time, not just the one tapped
                                 .opacity(((selectedFlag == number) || (selectedFlag == -1)) ? 1.0 : 0.25) // Animations challenge 2, change opacity to 0.25 for non tapped flags.  Better solution?
                                 .scaleEffect((selectedFlag == -1) ? 1 : ((selectedFlag == number ) ? 1.1 : 0.5))
-                                // Animation challenge 3, add more animations to non tapped flags.  Do not need additional .animation modifier for it to worl
+                                // Animation challenge 3, add more animations to non tapped flags.  Do not need additional .animation modifier for it to work
+                                .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                            
                         }
                     }
